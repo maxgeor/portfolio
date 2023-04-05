49d80aeda2ac35c8148791b2a458716efe129f4c
Max <73665502+maxgeor@users.noreply.github.com>
2023-04-04 19:33:02 -0600

    redesign

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				crimson: ['Crimson Text', 'serif'],
+				ibm: ['IBM Plex Sans', 'sans-serif'],
+				karla: ['Karla', 'sans-serif'],
+				lora: ['Lora', 'serif'],
+			}
+		},
 	},
 	plugins: [],
 }
