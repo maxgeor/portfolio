19b4286faf15de67b95dfd1f4f9a5d616a4b4088
Max <73665502+maxgeor@users.noreply.github.com>
2023-07-03 10:16:47 -0400

    .

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,7 +8,7 @@ module.exports = {
     },
 		extend: {
 			boxShadow: {
-				float: 'rgba(0, 0, 0, 0.065) 0px 1px 4px, rgba(0, 0, 0, 0.13) 0px 1px 2px;',
+				float: 'rgba(0, 0, 0, 0.055) 0px 1px 4px, rgba(0, 0, 0, 0.11) 0px 1px 2px;',
 			},
 			colors: {
 				gray: {
