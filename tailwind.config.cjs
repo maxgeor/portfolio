eeef1699c2b916ff699583b7957bac1706cce1fa
Max <73665502+maxgeor@users.noreply.github.com>
2023-07-24 13:07:23 -0400

    .

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -31,7 +31,7 @@ module.exports = {
 				xs: '450px',
 			},
 			boxShadow: {
-				sm: '0px 1px 1px -1px rgba(0,0,0,.05), 0px 2px 2px -1px rgba(0,0,0,.05), 0px 0px 0px 1px rgba(0,0,0,.0775)',
+				sm: '0px 1px 1px -1px rgba(0,0,0,.05), 0px 2px 2px -1px rgba(0,0,0,.05), 0px 0px 0px 1px rgba(0,0,0,.075)',
 				DEFAULT: '0px 1px 1px -1px rgba(0,0,0,.0975), 0px 2px 2px -1px rgba(0,0,0,.0975), 0px 0px 0px 1px rgba(0,0,0,.085)',
 				dark: '0px 0px 0px 0.5px rgba(0,0,0,.4), 0px 1px 1px -1px rgba(0,0,0,.12), 0px 2px 2px -1px rgba(0,0,0,.12), inset 0px 0.5px 0px hsla(0,0%,100%,.06), inset 0px 0px 1px 0px hsla(0,0%,100%,.16), inset 0px -6px 12px -4px rgba(0,0,0,.16)',
 				contour: 'inset 0px 0px 0px rgba(255,255,255,0.5)',
