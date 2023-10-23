29522d9832e93fd14f8c4313527aeeb4afe40117
Max <73665502+maxgeor@users.noreply.github.com>
2023-10-23 14:18:26 -0400

    optimized card images

--- src/utils/formatting.ts
+++ src/utils/formatting.ts
@@ -1,1 +1,4 @@
-export const toUrlSafeKebabCase = (s: string) => s.replace(/[^a-z0-9\s]/gi, '').replace(/\s+/g, "-").toLowerCase();
+export const toUrlSafeKebabCase = (s: string) => 
+  s.replace(/[^a-z0-9\s]/gi, '').replace(/\s+/g, "-").toLowerCase();
+
+export const stripExtension = (src: string) => src.replace(/\..*$/, ""); // video.mp4 -> video
