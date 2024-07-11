f035e9aa362c6313b856fc48d063582baa1b5e2b
Max <73665502+maxgeor@users.noreply.github.com>
2024-07-11 15:01:15 -0400

    .

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -67,7 +67,7 @@ module.exports = {
           50: '#FFFFFF',
           100: '#F5F5F5',
           150: '#f0f0f0',
-          200: '#eaeaea',
+          200: '#EDEDED',
           250: '#E6E6E6',
           300: '#dadada',
           400: '#909090',
