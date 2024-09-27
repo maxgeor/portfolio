d6a02f4f15cabf283a4f8f10d0befb32d06a5951
Max <73665502+maxgeor@users.noreply.github.com>
2024-09-27 16:48:12 -0400

    .

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,7 +10,7 @@ module.exports = {
     fontFamily: {
       sans: ['Arial', 'Helvetica Neue',  'Untitled Sans', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
       serif: ['Georgia', 'Times New Roman', 'Times', 'Bookish', , 'Cambria', 'serif'],
-      mono: ['IBM Plex Mono', 'Berkeley Mono', 'ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', 'Liberation Mono', 'Courier New', 'monospace'],
+      mono: ['Berkeley Mono', 'ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', 'Liberation Mono', 'Courier New', 'monospace'],
     },
     extend: {
       gap: {
