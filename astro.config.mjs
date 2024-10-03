d832e7ec6b49d76debed3eaf798acb363563580c
Max <73665502+maxgeor@users.noreply.github.com>
2024-10-03 19:43:44 -0400

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/static';
+// import vercel from '@astrojs/vercel/static';
 
 export default defineConfig({
   integrations: [mdx(), tailwind()],
-  output: 'static',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
+  // output: 'static',
+  // adapter: vercel({
+  //   webAnalytics: {
+  //     enabled: true,
+  //   },
+  // }),
 });
